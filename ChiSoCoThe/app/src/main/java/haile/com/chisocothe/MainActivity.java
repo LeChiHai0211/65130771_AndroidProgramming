ad3163ee62691dab9808856e21376be68d42e944
Le Chi Hai <hailehcl1@gmail.com>
2026-04-05 01:31:28 +0700

    Hoàn thánh App tính chỉ số cơ thể

--- ChiSoCoThe/app/src/main/java/haile/com/chisocothe/MainActivity.java
+++ ChiSoCoThe/app/src/main/java/haile/com/chisocothe/MainActivity.java
@@ -1,6 +1,8 @@
 package haile.com.chisocothe;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +15,33 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
+    public void TinhChiSo(View view){
+        EditText edtCN = (EditText) findViewById(R.id.idCanNang);
+        EditText edtCC = (EditText) findViewById(R.id.idChieuCao);
+        EditText edtKQ = (EditText) findViewById(R.id.idKetQua);
+
+        String strCN = edtCN.getText().toString();
+        String strCC = edtCC.getText().toString();
+
+        float fCN = Float.parseFloat(strCN);
+        float fCC = Float.parseFloat(strCC);
+
+        //Tinh toan
+        Float BMI = (float) (fCN / Math.pow(fCC, 2));
+
+
+        String strBMI = String.valueOf(BMI);
+        if(BMI < 18.5){
+            edtKQ.setText("Gầy");
+        } else if (BMI >= 18.5 && BMI <= 22.9) {
+            edtKQ.setText("Hoàn hảo");
+        }else if (BMI >= 23 && BMI <25){
+            edtKQ.setText("Thừa cân");
+        }else {
+            edtKQ.setText("Béo phì rồi đớt ít thôi!");
+        }
+    }
+
 }

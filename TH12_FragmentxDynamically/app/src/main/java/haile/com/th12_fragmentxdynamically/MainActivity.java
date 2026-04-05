4f4b6dc8864f17e3e12180a72c18b46505b518ee
Le Chi Hai <hailehcl1@gmail.com>
2026-04-05 18:12:16 +0700

    Hoàn thành TH12
    xong TH12

---
+++ TH12_FragmentxDynamically/app/src/main/java/haile/com/th12_fragmentxdynamically/MainActivity.java
@@ -0,0 +1,27 @@
+package haile.com.th12_fragmentxdynamically;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+
+                .add(R.id.IDframeContentHolder, new ContentFragment())
+                .add(R.id.IDfragmentFooter, new FooterFragment())
+
+                .commit();
+    }
+}

b57384d6f699354dddd728d966e9aa62fbb93342
Le Chi Hai <hailehcl1@gmail.com>
2026-04-20 07:43:53 +0700

    Thi giua ki commit1
    commit lan thu 1

--- ThiGiuaKi_65130771/app/src/main/java/thigk2/LeChiHai/thigiuaki_65130771/MainActivity.java
+++ ThiGiuaKi_65130771/app/src/main/java/thigk2/LeChiHai/thigiuaki_65130771/MainActivity.java
@@ -1,12 +1,18 @@
 package thigk2.LeChiHai.thigiuaki_65130771;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +20,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNavigationView);
+        bottomNav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+                int itemId = item.getItemId();
+                if(itemId == R.id.navigation_home){
+                    selectedFragment = new HomeFragment();
+                }
+                else if(itemId == R.id.navigation_chucnang1){
+                    selectedFragment = new ChucNang1Fragment();
+                }
+                else if(itemId == R.id.navigation_chucnang2){
+                    selectedFragment = new ChucNang2Fragment();
+                }
+                else if(itemId == R.id.navigation_chucnang3){
+                    selectedFragment = new ChucNang3Fragment();
+                }
+                else if(itemId == R.id.navigation_chucnang4){
+                    selectedFragment = new ChucNang4Fragment();
+                }
+
+                if(selectedFragment != null){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragmentSpace,selectedFragment).commit();
+                }
+
+                return true;
+            }
+        });
     }
 }

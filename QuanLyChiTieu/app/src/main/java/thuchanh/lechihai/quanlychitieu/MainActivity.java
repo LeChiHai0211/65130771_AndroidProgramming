bedea642a4be6835dfc181e7a8aad9c7404cfeff
Le Chi Hai <hailehcl1@gmail.com>
2026-05-04 09:13:34 +0700

    TH_vi du Quan Ly chi tieu
    TH QuanLyChiTieu

---
+++ QuanLyChiTieu/app/src/main/java/thuchanh/lechihai/quanlychitieu/MainActivity.java
@@ -0,0 +1,48 @@
+package thuchanh.lechihai.quanlychitieu;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.Firebase;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+        myRef.setValue("Hello, Le Chi Hai");
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String value = snapshot.getValue(String.class);
+                Toast.makeText(MainActivity.this,
+                        "Value: " + value,
+                        Toast.LENGTH_LONG).show();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(MainActivity.this,
+                        "Failed!",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
